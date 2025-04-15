a6653d11f8eca1e7d6f9f09b72bfc81b60b8315c
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2025-04-15 09:11:47 -0400

    Move get_os

--- R/cloudcompare.R
+++ R/cloudcompare.R
@@ -1,3 +1,20 @@
+get_os <- function()
+{
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
 #' Find CloudCompare executable path
 #'
 #' This function attempts to locate the CloudCompare executable based on the operating system.
